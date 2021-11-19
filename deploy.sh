366e83f16ab9be0e2d1dca5553c0d0c8b873290a
David Gallego Gómez <davidgallegogomez@yahoo.es>
2021-11-19 01:26:42 +0100

    ENH: Deploy without ssh added

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:DavidGallegoGomez/feliz-cumple-javo.git master:gh-pages
+git push -f https://github.com/DavidGallegoGomez/feliz-cumple-javo.git master:gh-pages
 cd -
